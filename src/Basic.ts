e7e40eddfebe08a34477fe987c2b5c7c868b50ab
uttamkrshaw <uttamkr5599@gmail.com>
2024-04-21 00:35:47 +0530

    Added Basic of TypeScript

---
+++ src/Basic.ts
@@ -0,0 +1,21 @@
+// String
+let myname: string = "My Name is Uttam Kumar Shaw";
+
+// Number
+let mynumber: number = 1520931909
+
+// Boolean
+let doyouownpsp: boolean = false
+
+// Array 
+// Array of String
+let dreamitems: string[] = ["Cars", "Laptops", "Iron Man Suits", "House"]
+let dream: Array<string> = ["Own bike & Cars", "Own a PS"]
+
+// Array of Number 
+let favNumbers: number[] = [5, 8, 9]
+let numbers: Array<Number> = [5,9,3]
+
+// Combine Array Of Different Types 
+let favCombos : (number | string)[]= ["Guns",32,"21Guns","DDD"]
+
